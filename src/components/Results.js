6be810211160eaeec43fda33028136efda254e38
Ayrat <gimranov45@gmail.com>
2022-09-13 14:30:16 -0700

    minor changes

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,8 +1,14 @@
+import { useState } from "react"
+
 
 const Results = (props) => {
+
+const [showModal, setShowModal] = useState(false)
+
   return (
     <>
       <h3>Here will be my search results</h3>
+      {/* modal will go here */}
       {props.results.map((item, idx) => <li key={idx}>
         {item.original_title}
       </li>)}
