f274c1b00bf98240f1ea9c31eac007ce70a5a363
Ayrat <gimranov45@gmail.com>
2022-09-13 13:54:39 -0700

    listing titles

--- src/components/Results.js
+++ src/components/Results.js
@@ -3,9 +3,9 @@ const Results = (props) => {
   return (
     <>
       <h3>Here will be my search results</h3>
-      {props.results.map(item => <p>
-        {item}
-      </p>)}
+      {props.results.map((item, idx) => <li key={idx}>
+        {item.original_title}
+      </li>)}
     </>
   )
 }
