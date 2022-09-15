eb9b516cda37d014d6115cff70261703b4beb6df
alice <aliceahuh213@gmail.com>
2022-09-15 19:33:53 -0400

    add edit review fetch call and delete review button

---
+++ src/components/EditReviewButton.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import { Button } from "react-bootstrap";
+import axios from "axios";
+
+
+
+const EditReviewButton = (props) => {
+
+const [reviews, setReviews] = useState(props.review)
+
+let reviewId = props.review._id
+console.log(reviewId)
+// add text below after setup the name 
+let text = "this is testing for edit review";
+let editedReview = {text, reviewId}
+
+    return (
+      <Button variant="primary" onClick={() => {   
+              axios.put(`${process.env.REACT_APP_SERVER}/reviews/edit?apiid=${props.selectedItem.apiId}`, editedReview )
+              .then((response) => {
+              console.log("connected", response.data);
+
+              })
+          
+      }}>Edit Review</Button>
+
+    )
+}
+
+
+
+export default EditReviewButton
