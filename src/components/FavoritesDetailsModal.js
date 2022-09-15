5284aaa539b71b08855bc6fa2adabdbb4bc5946f
DevHarleyA <aharleydev@gmail.com>
2022-09-15 16:41:38 -0400

    added delete movie button, modal for favorites pg

---
+++ src/components/FavoritesDetailsModal.js
@@ -0,0 +1,64 @@
+
+import ReviewButton from "./ReviewButton"
+import { Modal } from "react-bootstrap"
+import Button from "react-bootstrap/Button"
+import Image from 'react-bootstrap/Image'
+import Accordion from 'react-bootstrap/Accordion';
+
+const FavoritesDetailsModal = (props) => {
+
+
+    return (
+        <>
+            {/* grab the information from the results.js for that particular movie and display the information */}
+
+            <Modal show={props.showModal}>
+                <Modal.Header>
+                    <Modal.Title >{props.selectedItem.title}</Modal.Title>
+                </Modal.Header>
+                {props.selectedItem.backdrop_path != null
+                    ?
+                    <>
+                        <Image variant="top" className="img-thumbnail" src={`https://image.tmdb.org/t/p/w500${props.selectedItem.backdrop_path}`} />
+                    </>
+                    :
+                    <Image variant="top" className="img-thumbnail" src="no-image.png" />
+                }
+                <Modal.Body>
+                    { }
+                    <p className="g-1" ><span className="fw-bold">Release Date:</span> {props.selectedItem.release_date}</p>
+                    <p className="g-1" ><span className="fw-bold">Rating:</span> {props.selectedItem.vote_average}/10</p>
+                    {props.selectedItem.overview}
+                    {props.reviews.map((review, idx) =>
+                        <Accordion defaultActiveKey="idx">
+                            <Accordion.Item eventKey="0">
+                                <Accordion.Header>User Review</Accordion.Header>
+                                <Accordion.Body>
+                                    <p>{review.text}</p>
+                                    <span>Posted by: </span>
+                                    <span>{review.email}</span>
+                                </Accordion.Body>
+                            </Accordion.Item>
+                        </Accordion>
+                    )}
+                </Modal.Body>
+
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={props.modalOff}>
+                        Close Details
+                    </Button>
+                    {props.isAuth
+                        ?
+                        <>
+                            <ReviewButton selectedItem={props.selectedItem} user={props.user} />
+                        </>
+                        :
+                        <Button variant="secondary">Login to Save</Button>
+                    }
+                </Modal.Footer>
+            </Modal>
+        </>
+    )
+}
+
+export default FavoritesDetailsModal
---
+++ src/components/MovieDeleteButton.js
@@ -0,0 +1,9 @@
+import { Button } from "react-bootstrap";
+
+const MovieDeleteButton = () => {
+  return (
+    <Button variant="danger" className="ms-1">Delete</Button>
+  )
+}
+
+export default MovieDeleteButton
