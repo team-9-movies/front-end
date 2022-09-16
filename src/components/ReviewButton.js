c699f7b39baf1e4b121d3714a3110eeb0dfff170
DevHarleyA <aharleydev@gmail.com>
2022-09-16 01:05:31 -0400

    changed reviews views, added form for review post

--- src/components/ReviewButton.js
+++ src/components/ReviewButton.js
@@ -1,23 +1,29 @@
 import { Button } from "react-bootstrap";
 
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import axios from "axios";
 
 const ReviewButton = (props) => {
-let navigate = useNavigate()
-let userEmail = props.user.email
-let nickName = props.user.nickName
-// add text below after setup the name 
-let text = "this is testing";
-let newReview = {userEmail, nickName, text}
+
+
+
+  let navigate = useNavigate()
+  let userEmail = props.user.email
+  let nickName = props.nickName
+  // add text below after setup the name 
+  
+  let text = props.text
+  let newReview = { userEmail, nickName, text }
 
   return (
-    <Button variant="primary" onClick={() => {   
-            axios.put(`${process.env.REACT_APP_SERVER}/reviews?apiid=${props.selectedItem.apiId}`, newReview )
-            .then((response) => {
-            console.log("connected", response.data);
-            })
-        
+    <Button variant="primary" onClick={() => {
+      axios.put(`${process.env.REACT_APP_SERVER}/reviews?apiid=${props.selectedItem.apiId}`, newReview)
+        .then((response) => {
+          console.log("connected", response.data);
+          console.log(props.nickname)
+          console.log(props.text)
+        })
+
     }}>Add Review</Button>
   )
 }
