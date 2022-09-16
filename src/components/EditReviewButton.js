6e3528f2abaebcf46e2f0e72fd983da89272ae47
DevHarleyA <aharleydev@gmail.com>
2022-09-16 09:45:50 -0400

    review edit button & review form clear

--- src/components/EditReviewButton.js
+++ src/components/EditReviewButton.js
@@ -4,22 +4,37 @@ import axios from "axios";
 
 
 const EditReviewButton = (props) => {
-
-const [reviews, setReviews] = useState(props.review)
-
+// console.log(`Edit Review button props:`, props)
+// const [review, setReview] = useState(props.review)
+// useEffect(() => {
+//   setReview(props.review)
+// }, [props.review])
 
 let reviewId = props.review._id
 console.log(reviewId)
-// add text below after setup the name 
-let text = "this is testing for edit review";
-let editedReview = {text, reviewId}
 
     return (
-      <Button variant="primary" onClick={() => {   
-              axios.put(`${process.env.REACT_APP_SERVER}/reviews/edit?apiid=${props.selectedItem.apiId}`, editedReview )
+      <Button variant="primary" className="me-1" onClick={ () => { 
+              const text = window.prompt('What is your new review text?')
+              console.log(text)  
+              let editedReview = {text, reviewId}
+              console.log(editedReview)
+
+              axios.put(`${process.env.REACT_APP_SERVER}/reviews/edit?apiid=${props.selectedItem.apiId}`, editedReview)
+
               .then((response) => {
               console.log("connected", response.data);
-
+                // where we will set our reviews
+                const copyReviews = [...props.reviews]
+                const updatedReviews = copyReviews.map((review) => {
+                  if(review._id === reviewId) {
+                    review.text = text
+                    return review
+                  } else {
+                    return review
+                  }
+                })
+                props.setReviews(updatedReviews)
               })
           
       }}>Edit Review</Button>
